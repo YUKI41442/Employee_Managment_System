dfd24007b88a7061e665f0844b0be6ec77bf57ad
YUKI41442 <dresslyclothingcompany.com@gmail.com>
2024-11-28 14:18:29 +0530

    add employee controller

--- employee-manage/src/main/java/edu/nibm/crm/employeeController/EmployeeController.java
+++ employee-manage/src/main/java/edu/nibm/crm/employeeController/EmployeeController.java
@@ -1,10 +1,7 @@
 package edu.nibm.crm.employeeController;
 
 import edu.nibm.crm.dto.Employee;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +15,11 @@ public class EmployeeController {
     @PostMapping("add-employee")
     public void addEmployee(@RequestBody Employee employee) {
         employeeList.add(employee);
+    }
+
+    @GetMapping("get-all")
+    public List<Employee> getAll(){
+        return employeeList;
     }
 
 }
